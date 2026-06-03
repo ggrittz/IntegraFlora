88f12c163a3fe62baef7c4ce6483bb6815e9779a
Mali Oz <malicsalles@gmail.com>
2026-06-03 16:12:21 -0300

    removing unused files

--- R/remove_empty_cols.R
+++
@@ -1,8 +0,0 @@
-# Remove empty columns
-#
-# This will remove any column from a data.frame that is empty.
-remove_empty_cols <- function(x) {
-    empties <- sapply(x, function(c){all(is.na(c))})
-    x[,empties] <- NULL
-    x
-}
